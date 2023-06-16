4a0d4eb37a44b56b22c75fd99b1250a5e8b73758
natacat <nataly.jimenez.g@gmail.com>
2023-06-15 20:00:00 -0400

    primer commit

---
+++ js/about.05acaf9a.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunksegundo_proyecto_cli"]=self["webpackChunksegundo_proyecto_cli"]||[]).push([[443],{507:function(n,t,u){u.r(t),u.d(t,{default:function(){return i}});var e=function(){var n=this;n._self._c;return n._m(0)},s=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],c=u(1),l={},r=(0,c.Z)(l,e,s,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=about.05acaf9a.js.map
